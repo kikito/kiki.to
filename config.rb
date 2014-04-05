c3538e70f1855e3b04cdb4875cf7ec152d3bace2
kikito <kikito@gmail.com>
2014-04-05 02:54:44 +0200

    monkeypatch middleman-google-analytics to use the latest analytics.js

--- config.rb
+++ config.rb
@@ -70,6 +70,7 @@ configure :build do
 
   activate :google_analytics do |ga|
     ga.tracking_id = 'UA-49731031-1'
+    ga.domain_name = 'kiki.to'
   end
 
   # Minify Javascript on build
@@ -122,5 +123,24 @@ helpers do
 
   def feed_title
     "#{site_title} - Atom Feed"
+  end
+
+  def google_analytics
+    options = ::Middleman::GoogleAnalytics.options
+    tracking_id = options.tracking_id
+    domain_name = options.domain_name
+    if tracking_id
+      %{
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', '#{tracking_id}', '#{domain_name}');
+        ga('send', 'pageview');
+      </script>
+      }
+    end
   end
 end
